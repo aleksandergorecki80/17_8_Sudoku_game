f4d5d9c84c68b8314044636dd9cf20e5087a8854
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2018-07-10 10:23:49 +0100

    Add Title comonent

---
+++ src/components/Board.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Title from "./Title";
+
+class Board extends Component {
+  render() {
+    return (
+    <div className="Board">
+            
+       <p>board</p>
+       <Title />
+    </div>
+    );
+  }
+}
+
+
+
+export default Board;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,22 @@
 import uuid from "uuid";
 import { hot } from "react-hot-loader";
+import Board from "../components/Board"
 
 import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        kkikkk
-      </div>
+		<div className="App">
+		   <h1>Sudoku</h1>
+		   <Board />
+		   <div className="buttons">
+		       <button>Check</button>
+		       <button>New Game</button>
+		       <button>Solve</button>
+		       <button>Restart</button>
+		   </div>
+		</div>
     );
   }
 }
